59ed04cfe68d9cc31b4d2cd57bf2c12ce443ef32
yichung <ql3895619@gmail.com>
2019-01-05 03:05:30 +0800

    init cache

--- csim.c
+++ csim.c
@@ -1,4 +1,21 @@
 #include "cachelab.h"
+#include "csim.h"
+
+Cache cache_create(int s, int E, int b){
+    Cache cache = malloc(sizeof(Cache));
+    
+    long S = 2 << s;
+    long B = 2 << b;
+
+    cache->sets = (Set *)calloc(S, sizeof(Set));
+    
+    for(long i = 0; i < S; i++){
+        cache->sets[i] = (Cacherow *)calloc(E, sizeof(Cacherow));
+        cache->sets[i]->block = (char *)calloc(B, sizeof(char));
+    }
+
+    return cache;
+}
 
 int main()
 {
---
+++ csim.h
@@ -0,0 +1,21 @@
+#ifndef _CSIM_H
+#define _CSIM_H
+
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef struct {
+  bool valid;                // valid bit 
+  usigned long long tag;     // tag size = 64-(s+b)
+  char *block                // array of bytes
+} Cacherow;
+
+typedef struct {
+    Cacherow *cacherow;      // array of rows
+} Set;   
+
+typedef struct {
+    Set *sets;   // array of Set
+}Cache;
+
+Cache cache_create(int s, int E, int b);
