6bce7231cafe075193508c6bd83a6d9b8890491a
yenpo <asd757817@gmail.com>
2019-01-05 09:50:16 +0800

    Add function to parse user inputs.
    Use getopt function to parser user inputs.
    Add gitignore.
    
    Signed-off-by: yenpo <asd757817@gmail.com>

--- csim.c
+++ csim.c
@@ -1,7 +1,33 @@
 #include "cachelab.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+int s,E,b;
 
-int main()
+int main(int argc, char * argv[])
 {
+    char c ;
+    while((c=getopt(argc, argv, "hvs:E:b:t:")) != -1)
+    {
+        switch(c)
+        {
+            case 'h':
+                break;
+            case 'v':
+                break;
+            case 's':
+                s = atoi(optarg);
+                break;
+            case 'E':
+                E = atoi(optarg);
+                break;
+            case 'b':
+                b = atoi(optarg);
+                break;
+            case 't':
+                break;
+        }
+    }
     printSummary(0, 0, 0);
     return 0;
 }
